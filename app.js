50ad1e808be9f9fe692ac4840c0de1ec79db66a4
mohmmad701 <mohammadriziq70@yahoo.com>
2021-06-01 11:22:35 +0300

    entrance301

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+let exsitlocalstorge=[];
+function Trip(name,trip,transport)
+{
+    this.name=name;
+    this.trip=trip;
+    this.transport=transport;
+
+}
+let from1=document.getElementById('form');
+from1.addEventListener('submit',handlesubmit);
+
+function handlesubmit(event)
+{
+    event.preventDefault();
+    l
+}
